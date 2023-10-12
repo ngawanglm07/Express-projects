9b1f6c6d86f7519c1d3d8b32ba56868865fb4cb2
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-10-12 21:15:58 +0530

    added light version of post put patch and delete and now checking it on postman

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+// get request
 app.get('/' , (req,res) => {
     res.send('hello')
 })
@@ -13,6 +15,29 @@ app.get('/about', (req,res)=>{
 app.get('/contact', (req,res)=>{
     res.send('hello this is the contact page')
 })
+
+
+
+
+app.post('/register' , (req,res) =>{
+ res.sendStatus(201);
+})
+
+app.put('/user/ngawang' , (req,res) =>{
+    res.sendStatus(200);
+})
+
+app.patch('/user/ngawang' , (req,res)=>{
+    res.sendStatus(200);
+})
+
+app.delete('/user/ngawang', (req,res)=>{
+    res.sendStatus(200);
+})
+
+
+
+
 
 app.listen(port , () =>{
   console.log(`server is running at port 3000 ${port}`)
